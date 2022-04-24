292054d5720ec68b71ea0a949259bc1031b5f132
Ruslan Kudlay <Kudlayra@gmail.com>
2022-04-24 20:27:36 +0500

    Initial commit

---
+++ app/src/main/java/com/example/affirmations/model/Affirmation.kt
@@ -0,0 +1,9 @@
+package com.example.affirmations.model
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+data class Affirmation (
+    @StringRes val stringResourceID: Int,
+    @DrawableRes val imageResourceId: Int) {
+}
